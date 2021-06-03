fa373573b6ddcb12fd4a1b1045fbd6ea1a3581b7
ROSHNA-301174285 <rraju14@my.centennialcollege.ca>
2021-06-03 17:51:40 -0400

    background and image changes

--- Routes/index.js
+++ Routes/index.js
@@ -6,9 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 exports.default = router;
-router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Home', page: 'home' });
-});
 router.get('/home', function (req, res, next) {
     res.render('index', { title: 'Home', page: 'home' });
 });
